8dab5cb6a2ebb61959c3cb4a52c855674ba06113
Baiketli Dijo <baiketlid@gmail.com>
2024-11-22 23:47:05 +0200

    Added JS runtime extensions

---
+++ LearnBlazor/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Extensions;
+
+public static class IJSRuntimeExtensions
+{
+    public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+    {
+        await jSRuntime.InvokeVoidAsync("showToastr", "success", message);
+    }
+
+    public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+    {
+        await jSRuntime.InvokeVoidAsync("showToastr", "error", message);
+    }
+}
