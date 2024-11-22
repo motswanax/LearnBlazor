ce1f0c7e92052abde155eda22e03ca5aaf815df0
Baiketli Dijo <baiketlid@gmail.com>
2024-11-22 09:54:20 +0200

    Added JS to the project.

---
+++ LearnBlazor/wwwroot/js/common.js
@@ -0,0 +1,26 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}  
+
+window.ShowSwal = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            title: "Task Failed!",
+            text: message,
+            icon: "error"
+        });
+    }
+
+}
