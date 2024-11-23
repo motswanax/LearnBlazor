d9a01e635b8cfa56a9ed958a6e0457ee54687485
Baiketli Dijo <baiketlid@gmail.com>
2024-11-23 22:33:39 +0200

    Added form handling

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -1,10 +1,26 @@
-﻿namespace LearnBlazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models;
 
 public class Product
 {
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
     public bool IsActive { get; set; }
+
+    [Range(1, 1000)]
     public decimal Price { get; set; }
     public List<ProductProp> ProductProperties { get; set; }
+
+    public Category Category { get; set; }
+    public DateOnly AvailableAfter { get; set; }
+}
+
+public enum Category
+{
+    Entree,
+    Appetizer,
+    Dessert
 }
